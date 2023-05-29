6a3b25394467eba6c8ccf7d4caba4e9907b56f87
Rodrigo da Silva <rodrigoryan618@gmail.com>
2023-05-29 16:22:45 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,428 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simplesmente Te Amo</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css"/>
+    
+
+</head>
+
+<body>
+
+<header>
+
+    <div class="tema">
+        <center><h1>Não consegui fazer um header, desculpa, mas é de coração... S2</h1></center>
+    </div>
+
+
+</header>
+
+<div class="subheader">
+    <p style = "font-family:Courier New;">
+        Primeiramente a explicação do porque a inadagação "Oque eu vejo em você para ser tudo isso..." é tão sem sentido!!!
+    </p>
+</div>
+
+
+    <div class="card">
+        <div class="midia">
+            <audio autoplay="autoplay" controls="controls">
+                <source src="midias/beijo.mp3" type="audio/mp3" />
+            </audio>
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 06 de Março de 2023 | 17:58  --- </span></div>
+
+             <p class="justify">
+                Bem, isso foi logo no começo, mas eu já tinha uma grande leve desconfiança de que você 
+                seria a mulher que eu chamaria de namorada.
+                </br></br>
+                Para você, esse áudio pode não simbolizar nada, mas foi a primeira vez que você disse a 
+                "simples" palavra >>> BEIJO <<< para mim. E sim, isso me marcou, pois de uma maneira 
+                estranha isso me alegrou, já que a ideia de um beijo seu aquecia meu coração. 
+            </p>
+
+        </div>
+
+    </div>
+
+    <div class="card">
+        <div class="midia">
+        <audio autoplay="autoplay" controls="controls">
+            <source src="midias/esclarecimento.mp3" type="audio/mp3" />
+        </audio>
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 22 de Março de 2023 | 00:31 --- </span></div>
+
+             <p class="justify">
+                Isso, sem sombra de dúvidas, sempre será um dos meus áudios preferidos da minha vida, pois você me 
+                fez perceber que naquele instante, você estava de peito aberto, falando que as coisas que poderiam 
+                me afligir não eram para ser tratadas como coisas ruins. Você deixou claro que os espaços de tempo 
+                em que não nos falamos durante o dia não eram porque você não queria falar comigo, ou como você bem 
+                disse, "não estava pensando em você", mas sim porque você estava ocupada.
+            
+                </br></br>
+
+                Quando eu digo que estar com você me ensina muito mais do que qualquer coisa na vida, é sobre isso. 
+                Aprendi que o amor não se trata de estar <b>"GRUDADO"</b> o dia todo, mas sim de <b>PENSAR</b> um no outro 
+                o dia inteiro e desejar o bem-estar do outro durante todo o tempo. Após esse áudio, percebi que os 
+                momentos em que você tinha disponibilidade e dedicava esse tempo do seu dia a mim tinham um peso 
+                <b>ENORME</b>. Por isso, valorizo cada instante livre que você "disponibiliza" para mim.
+
+            </p>
+
+        </div>
+
+    </div>
+
+    <div class="card">
+        <div class="midia">
+            <img src="midias/inseguranças.png" alt="" width="490">
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 22 de Março de 2023 | 11:58 --- </span></div>
+
+             <p class="justify">
+                
+                Aqui você >>> <b>MATOU</b> <<< um dos meus maiores medos, que era o de você achar minhas mensagens irritantes 
+                de alguma forma, mas muito pelo o contrario, você fez mais uma vez eu percber que você valoriza até as mínimas 
+                coisas que eu faço, do Bom dia ao Boa Noite... 
+            
+                </br></br> 
+                
+                EU AINDA PRECISO DE MAIS MOTIVOS DE SER QUEM EU SOU PARA VOCÊ??? ( SEGUE A LISTA AINDA )
+
+                </br></br> 
+
+                ( Teve inúmeras vezes que você me fez percebr isso mas eu precisava de um exemplar )
+            </p>
+
+        </div>
+
+    </div>
+
+    
+    <div class="card">
+        <div class="midia">
+        <audio autoplay="autoplay" controls="controls">
+            <source src="midias/ansiedade.mp3" type="audio/mp3" />
+        </audio>
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 24 de Março de 2023 | 12:49 --- </span></div>
+
+             <p class="justify">
+                Esse áudio pode parecer que fala mais de você em relação a mim, e realmente pode falar, mas o que 
+                isso representou para mim foi de uma grandeza imensurável. Na minha vida, nunca tive uma pessoa 
+                que fosse o refúgio para minha alma, e saber que, da pessoa que mais amo, somos a paz um do outro, 
+                me toca profundamente. Saber que tranquilizo aquele que me tranquiliza é um dos grandes sinais de 
+                que estou no caminho certo na vida.
+                
+                <br></br>
+
+                Pois já houve inúmeras noites em que eu estava em minha completa ruína, e >>> VOCÊ <<< foi a 
+                pessoa que conseguiu arrancar de mim o sorriso mais sincero que já tive. Você É e SEMPRE SERÁ 
+                a minha calmaria.
+
+                <br></br>
+                
+                <center> ( são 04:29 e to digitando isso chorando ) </center>
+
+            </p>
+
+        </div>
+
+    </div>
+
+
+    <div class="card">
+        <div class="midia">
+            <img src="midias/futuro.png" alt="" width="490">
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 01 de Abril de 2023 | 21:21 --- </span></div>
+
+             <p class="justify">
+                
+               Vou tentar aqui ao máximo te passar o quanto essa mensagem tem importância na minha vida e o impacto 
+               que ela tem em mim.
+
+               <br></br>
+
+               FUTURO
+               </br>
+                Um questão que não gostamos muito de falar sobre, pois é de nós dois vivermos o hoje "sem" preocupações 
+                do amanhã, mas que você sabe muito bem que não muito o meu forte ( KKKKKKKKKKKKK ), posso não comentar, mas sempre gosto 
+                de planejar tudo, incluindo a possibilildade de dar errado certas coisas, e é por isso que não quero que 
+                se preocupe, e quero leve tudo isso na maneira bonita de se ver a vida, pois foi dessa forma que você me fez 
+                enxergar o mundo.
+
+                <br></br>
+
+                Sempre fui muito de idealizar o amanhã, SEMPRE. E continuo sendo, porém de uma maneira diferente.
+                Pois dessa vez eu não idealizo o amanhã com o fator da preocupação e sim com o da traquilidade, 
+                pois foi em você que percebi que o futuro não precisa ser vivido antes, de maneira ansiosa, e sim 
+                deve ser construído uma base sólida, para quando ele chegue, estejamos preparados. E é exatamente 
+                isso que estamos fazendo, estamos nos aperfeiçoando individualmente, para que quando o futuro  que nos 
+                aguarda chegar, estejamos prontos. 
+
+                <br></br>
+
+                E caso ainda não tenha intendido o motivo disso ser tão importante para mim, mesmo que você falasse 
+                para mim o quanto gosta de como eu enxergo o mundo e tudo mais, mas É VOCÊ que merece todo esse merito, 
+                pois foi você que mudou a forma de como eu encarar tanto o presente quanto a visão que tenho do futuro.
+
+            </p>
+
+        </div>
+
+    </div>
+
+
+
+    <div class="card">
+        <div class="midia">
+            <img src="midias/euteamo.png" alt="" width="490">
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 04 de Abril de 2023 | 21:53 --- </span></div>
+
+            <p class="justify">
+              Momento que não preciso explicar muita coisa, pois primeiramente, eu não conseguiria explicar em palavras
+              oque eu senti naquele momento, Mas eu sabia de uma coisa, naquele momento eu tive a confirmação de uma 
+              certeza que eu já tinha... ERA VOCÊ, É VOCÊ E SEMPRE SERÁ VOCÊ...
+
+                </br></br>
+            </p>  
+
+              <div class="center"><b style="color:red;"> EU TE AMO!!! </b></div>
+
+                </br></br>
+             <p class="justify">
+
+                ( Ai amor, juro que mão aguentando mais fazer essa lista, eu to chorando desde de ontem 28/05, a cada item 
+                eu choro por tipo uns 20 consecutivos, a cada momento percebo ainda mais que te amo, e isso só aumenta...
+                )
+
+             </p>   
+              
+
+                </br></br>
+
+              Vou roubar uma das frases de um dos vídeos que estão na sua playlist mas é pelo em do contexto
+              
+                </br></br>
+
+              <div class="center"><i>" O meu coração já tem tanto de você que já mal posso chamá-lo de meu."</i></div>
+                </br></br>
+            <p class="justify">
+              Você reservou um espaço tão grande no meu coração e na minha mente que  meus dias se iniciam e se encerram 
+              com pensamentos em você... Em TODAS as mínimas coisas legais que acontecem no meu dia instantaneamente já 
+              penso em compartilhar tais coisassituações com você, pois já não quero mais pensar em não compartilhar momentos 
+              com você...
+
+            </p>
+
+        </div>
+
+    </div>
+
+    <div class="card">
+        <div class="midia">
+            <img src="midias/analogia.png" alt="" width="490">
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 05 de Abril de 2023 | 11:19  --- </span></div>
+
+            <p class="justify">
+                Preciso realmente falar algo???
+                </br></br>
+                (Não exclusivamente nessa mensagem mas especialmente nela, você fez eu me sentir o homem mais sortudo desse mundo, 
+                pois é nela que vocé conseguiu reunir todas palavras que me fazem chorar por horas consecutivas, é nela que percebi mais 
+                do quê no dia anterior e menos do que o dia sucessor que era com você que gostaria de compartilhar momentos inesquecíveis.)
+                </br></br>
+                E saber que de certa forma eu te fiz enxergar um melhor versão do mundo me faz querer melhorar a cada dia mais, pois eu quero ser 
+                melhor para lhe mostrar cada vez mais o que o mundo tem de melhor a nos exibir e nos oferecer...
+                </br></br>
+                ( E um detalhe bobo mais que eu AMO muito é que desde sempre tu sempre gostou das analogias bobas que eu sempre fiz... )
+            </p>
+        </div>
+
+    </div>
+
+    <div class="card">
+        <div class="midia">
+            <img src="midias/preocupação2.png" alt="" width="490">
+            <img src="midias/preocupação.png" alt="" width="490">
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 18 de Abril de 2023 | 07:32 e 22:19  --- </span></div>
+
+            <p class="justify">
+                Momentos que deixaram ainda mais óbvio o quanto você se preocupa tanto com meu bem estar quanto 
+                a forma que eu me sinto em relação a você, momentos que sim me fizeram eu chorar MUITO, pois para 
+                mim é uma honra inigualável ser motivo de amor e preocupação por uma uma pessoa tão incrivel quanto você...
+                </br></br>
+                Sim sou muito chorão, mas é aliviante por tudo isso que sempre quis demonstrar mas estava preso no meu peito, 
+                e devido a uma <b style="color:green;"> GRANDE SORTE </b> estou tendo a honra e a gloria de ser com você, a pessoa 
+                que mais amei e que me ensinou oque é um amor tranquilo e calmo.
+            </p>
+            
+
+        </div>
+
+    </div>
+
+
+    <div class="card">
+        <div class="midia">
+            <img src="midias/sintonia.png" alt="" width="490">
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 26 de Abril de 2023 | 16:36  --- </span></div>
+
+            <p class="justify">
+                Ser recíproco, um dos nossos grandes diferenciais, sempre na medida de nossos limites 
+                sempre estamos demonstrando da maior e melhor maneira possivel, cada um do seu jeitinho, 
+                da sua maniermaneira de ser, com suas limitações e difrenciais, e  é exatamente isso que faz 
+                eu me apaixonar cada dia mais pela pessoa incrivel que você é. As diferenças. 
+            </p>
+                </br></br>
+                <div class="center"><i>As coincidências me conquistaram mas as diferenças me fizeram amar</i></div>
+                </br></br>
+
+            <p class="justify">
+                
+                Foi em você que achei as palavras mais sinceras, o pouco tempo mais bem distribuido e a prioridade 
+                certa que podia ser dada, dada nos momentos certos. Como até já falei/falamos, tenho a leve grande convicção 
+                que tudo que vivi/vivemos até hoje foi por estes momentos e com isso tudo que antecede se torna vago e sem sentido, 
+                foi tudo por este momento... E ainda vou além, se eu precisasse passar por tudo mil vezes pior mas eu 
+                tivesse a certeza que no final eu teria este mesmo desfecho. 
+                
+               <div class="center"><b>eu não hexitaria em aceitar.</b> </div>
+
+            </p>
+
+                </br></br>
+                <div class="center"><i> <b>MESMA SINTONIA </b> </i></div>
+                </br></br>
+
+
+            <p class="justify">
+
+            Passamos por alguns dias bem dificeis com essa tematica, mas se por um acaso pergutanssem se aquilo em algum momento me 
+            deixou com medo, com total certeza eu diria... <b style="color:RED;">SIM</b> ( KKKKKKKKKKKKK )
+
+            <br><br>
+
+            Mas não esse medo que talvez você esteja pensando, e sim medo de em algum momento você achar que em algum momento, em 
+            alguma circunstancia, alguma ocasião, em alguma situação ou sla algum alguma coisa, você não fosse o suficiente, e mesmo 
+            você estando naquele momento bem dificil se pos a se preocupar comigo e se isso estaria fazendo bem a mim. E aqui eu posso 
+            falar com total certeza do fatos:
+
+            <br><br>
+
+            <b>
+                VOCÊ É, SEMPRE FOI E LEVANDO EM CONSIDERAÇÃO OS PADRÕES DO DIA-A-DIA ( KKKKKKKKKKK ), SEMPRE VAI SER A MINHA PESSOA FAVORITA, 
+                A PESSOA QUE ME SUPRE EM TUDO, A PESSOA QUE MAIS AMO, A PESSOA COM QUEM MAIS ME IMPORTO, O SER NO QUAL HABITA EM MINHA MENTE DO 
+                BOM DIA AO BOA NOITE, A PESSOA QUE QUERO COMPARTILHAR MOMENTOS INESQUECIVEIS NA MINHA VIDA, VOCÊ É PESSOA QUE ESCOLHI 
+                E EU SOU A QUE VOCÊ ESCOLHEU, E O UNIVERSO NOS UNIU. 
+            </b>
+
+            </p>
+        </div>
+
+    </div>
+
+
+
+
+    <div class="card">
+        <div class="midia">
+            <img src="midias/aliança.jpg" alt="" width="300">
+        </div>
+
+        <div class="content">
+
+             <div class="pdd"><span> --- 29 de Abril de 2023 | 18:04  --- </span></div>
+
+            <p class="center">
+                O dia que ficará marcado da história...
+            </p>
+            <br><br>
+            <div class="center"></div><b><i>O COMEÇO DE TUDO ( SÓ QUE COM NOME AGORA KKKKK )</i></b>
+            <br>
+            <div class="center"><b><i>Enfim namorados, oque era para ser apenas uma casa de praia...</i></b></div>
+
+        </div>
+
+    </div>
+
+</body>
+
+
+
+    <div class="footerLuck">
+
+        <div class="cases">
+
+            <div class="case0">
+                <span>Em caso e algum problema ligue ao SAN ( Sistema de Atendimento a Namorada ) </span>
+                <br>
+                <span>(85)98636-3930</span>
+            </div>
+            
+            <div class="case1">
+
+            <a href="https://vm.tiktok.com/ZM2Yt1p3j/">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tiktok" viewBox="0 0 16 16">
+                    <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z"/>
+                </svg>
+            </a>
+
+            </div>
+
+            <div class="case2">
+
+            <a href="https://instagram.com/rodrick_1320?igshid=ZDc4ODBmNjlmNQ==">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                </svg>
+            </a>
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="case4">
+        <div class="span"><span> © 2023 Copyright: Amor da sua vida </span></div>
+    </div>
+
+
+</html>
